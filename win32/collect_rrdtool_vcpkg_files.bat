9d906fd57ea6435bba6520a5b41ba8bdb769be0a
Shivam Mathur <shivam_jpr@hotmail.com>
2024-07-22 13:58:56 +0000

    Test [skip ci]

--- win32/collect_rrdtool_vcpkg_files.bat
+++ win32/collect_rrdtool_vcpkg_files.bat
@@ -24,45 +24,19 @@ SET release_dir=%base_dir%\win32\nmake_release_%1_vcpkg\rrdtool-%version%-%1_vcp
 echo release_dir: %release_dir%
 
 if exist %base_dir%\win32\nmake_release_%1_vcpkg rmdir %base_dir%\win32\nmake_release_%1_vcpkg /s /q
-mkdir %release_dir%
+mkdir -p %release_dir%\files
 
 REM use xcopy instead of copy. xcopy creates directories if necessary and outputs the copied file
 REM /Y Suppresses prompting to confirm that you want to overwrite an existing destination file.
 REM /D xcopy copies all Source files that are newer than existing Destination files
 
-xcopy /Y /D %base_dir%\win32\librrd-8.dll %release_dir%
-xcopy /Y /D %base_dir%\win32\rrdcgi.exe %release_dir%
-xcopy /Y /D %base_dir%\win32\rrdtool.exe %release_dir%
-xcopy /Y /D %base_dir%\win32\rrdupdate.exe %release_dir%
+dir %base_dir%\win32\
+
+xcopy /Y /D /E %base_dir%\win32\* %release_dir%\files
 
 REM The following part needs to be checked and maintained after an update to a new vcpkg version
 REM Names of dlls can change over time, which has happened in the past
 REM e.g. glib-2.dll -> glib-2.0-0.dll, expat.dll -> libexpat.dll
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\brotlicommon.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\brotlidec.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\bz2.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\cairo-2.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\libexpat.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\ffi-8.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\fontconfig-1.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\freetype.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\fribidi-0.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\gio-2.0-0.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\glib-2.0-0.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\gmodule-2.0-0.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\gobject-2.0-0.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\harfbuzz.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\iconv-2.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\intl-8.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\libpng16.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\libxml2.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\liblzma.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\pango-1.0-0.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\pangocairo-1.0-0.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\pangoft2-1.0-0.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\pangowin32-1.0-0.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\pcre2-8.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\pixman-1-0.dll %release_dir%
-xcopy /Y /D %base_dir%\vcpkg\installed\%1-windows\bin\zlib1.dll %release_dir%
+xcopy /Y /D /E %base_dir%\vcpkg\installed\%1-windows\* %release_dir%
 
 popd
